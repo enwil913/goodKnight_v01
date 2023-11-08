400af4d2cc4e131c8927c137109daa0b6a891c5b
enwil913 <enwil913@gmail.com>
2023-11-08 13:15:58 +0800

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+
+
+const Header = ({title}) => {
+  return (
+    <header>
+        <h1 style={headingStyle}>{title}</h1>
+      
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Default Title'
+}
+
+Header.PropTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+const headingStyle = {
+    color: 'red'
+}
+
+export default Header
