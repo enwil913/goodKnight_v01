eee87755dda3687e7a54d58118c992e1ffca40f2
enwil913 <enwil913@gmail.com>
2023-11-15 16:48:45 +0800

    202231115

--- gitPush.sh
+++ gitPush.sh
@@ -2,7 +2,7 @@
 
 git status
 git add --all
-git commit -m "202231114"
+git commit -m "202231115"
 git push
 echo Press Enter...
 read
--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
-import {useState} from 'react'
+import {useState, useEffect } from 'react'
 import React from 'react'
 import Header from "./components/Header";
 import Trainers from './components/Trainers';
 
 
+
 function App() {
-  const [trainers, setTrainers] = useState([
-    {
-      id: 1,
-      name: 'ABC',
-      shortName: 'A',
-      totalWin: 0,
-      winHistory: [],
-    
-    },
-    {
-      id: 2,
-      name: 'DEF',
-      shortName: 'D',
-      totalWin: 0,
-      winHistory: [],
-    
-    },
-    {
-      id: 3,
-      name: 'GHI',
-      shortName: 'G',
-      totalWin: 0,
-      winHistory: [],
-   
+  const [trainers, setTrainers] = useState([])
+
+  useEffect(() => {
+    const getTrainers = async () => {
+      const trainersFromServer = await fetchTrainers()
+      setTrainers(trainersFromServer)
     }
-  ])
+    getTrainers()
+  }, [])
+
+  const fetchTrainers = async () => {
+    const res = await fetch('https://json-server-vercel-khaki.vercel.app/trainers')
+    const data = await res.json()
+    return data
+  }
 
   return (
     <div className="container">
@@ -41,4 +31,31 @@ function App() {
 }
 
 export default App;
- 
+
+
+  // const [trainers, setTrainers] = useState([
+  //   {
+  //     id: 1,
+  //     name: 'ABC',
+  //     shortName: 'A',
+  //     totalWin: 0,
+  //     winHistory: [],
+    
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'DEF',
+  //     shortName: 'D',
+  //     totalWin: 0,
+  //     winHistory: [],
+    
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'GHI',
+  //     shortName: 'G',
+  //     totalWin: 0,
+  //     winHistory: [],
+   
+  //   }
+  // ])
